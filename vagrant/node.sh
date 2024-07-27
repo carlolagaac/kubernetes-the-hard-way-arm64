0648b82c2d1f490bc75b5e4d170643f957470a0a
carlo <carlo>
2024-07-27 23:58:03 +0800

    testing deployment

--- vagrant/node.sh
+++ vagrant/node.sh
@@ -3,6 +3,8 @@
 apt-get -y update 
 apt-get -y upgrade 
 
+apt-get -y install socat conntrack ipset
+
 sed -i \
   's/^#PermitRootLogin.*/PermitRootLogin yes/' \
   /etc/ssh/sshd_config
@@ -16,6 +18,11 @@ cd /vagrant
 cp id_rsa.pub /root/.ssh/authorized_keys
 cp id_rsa /root/.ssh/id_rsa
 chmod 600 /root/.ssh/authorized_keys
+
+swapon --show
+swapoff -a
+
+sed -i '/^\/dev\/mapper\/debian--12--vg-swap_1/s/^/#/' /etc/fstab
 
 
 
